4fa5fe3032af8d39ba2fbe210f0c25fe6ff8e9e0
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-04-14 21:26:16 +0300

    done

--- WpfApp77/MainWindow.xaml.cs
+++ WpfApp77/MainWindow.xaml.cs
@@ -1,24 +1,43 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApp77
+namespace Moderator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ModerateText_Click(object sender, RoutedEventArgs e)
+        {
+            string textFilePath = TextFilePathTextBox.Text;
+            string moderationFilePath = ModerationFilePathTextBox.Text;
+
+            if (File.Exists(textFilePath) && File.Exists(moderationFilePath))
+            {
+                string[] moderationWords = File.ReadAllLines(moderationFilePath);
+                string[] lines = File.ReadAllLines(textFilePath);
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    foreach (string word in moderationWords)
+                    {
+                        lines[i] = lines[i].Replace(word, new string('*', word.Length));
+                    }
+                }
+
+                File.WriteAllLines(textFilePath, lines);
+
+                OutputTextBlock.Text = "Moderation complete.";
+            }
+            else
+            {
+                OutputTextBlock.Text = "File paths are invalid.";
+            }
         }
     }
 }
