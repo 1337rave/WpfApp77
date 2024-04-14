834141307571a4b0262bae4e6c2f8dd52a65d4d9
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-04-14 21:38:07 +0300

    done

--- WpfApp77/MainWindow.xaml.cs
+++ WpfApp77/MainWindow.xaml.cs
@@ -1,24 +1,76 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApp77
+namespace FileStatistics
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GetFileStatistics_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = FilePathTextBox.Text;
+
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    string[] lines = File.ReadAllLines(filePath);
+                    int sentenceCount = lines.Length;
+                    int uppercaseCount = 0;
+                    int lowercaseCount = 0;
+                    int vowelCount = 0;
+                    int consonantCount = 0;
+                    int digitCount = 0;
+
+                    foreach (string line in lines)
+                    {
+                        foreach (char c in line)
+                        {
+                            if (char.IsUpper(c))
+                            {
+                                uppercaseCount++;
+                            }
+                            else if (char.IsLower(c))
+                            {
+                                lowercaseCount++;
+                                if ("aeiouAEIOU".IndexOf(c) != -1)
+                                {
+                                    vowelCount++;
+                                }
+                                else
+                                {
+                                    consonantCount++;
+                                }
+                            }
+                            else if (char.IsDigit(c))
+                            {
+                                digitCount++;
+                            }
+                        }
+                    }
+
+                    StatisticsTextBlock.Text = $"Number of sentences: {sentenceCount}\n" +
+                                                $"Number of uppercase letters: {uppercaseCount}\n" +
+                                                $"Number of lowercase letters: {lowercaseCount}\n" +
+                                                $"Number of vowels: {vowelCount}\n" +
+                                                $"Number of consonants: {consonantCount}\n" +
+                                                $"Number of digits: {digitCount}";
+                }
+                else
+                {
+                    StatisticsTextBlock.Text = "File not found.";
+                }
+            }
+            catch (Exception ex)
+            {
+                StatisticsTextBlock.Text = $"Error: {ex.Message}";
+            }
         }
     }
 }
